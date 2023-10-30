6f47258d7e3f0659cf0bae8ec052f7a714ad0a01
KatiaGrasso <101561044+KatiaGrasso@users.noreply.github.com>
2023-10-30 19:11:34 +0100

    @Composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+}
+android {
     namespace = "com.example.shoppinglist"
     compileSdk = 34
 
@@ -37,7 +46,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -45,4 +54,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.compose.ui:ui:1.5.4")
+    implementation ("androidx.activity:activity-compose:1.8.0")
+    // implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-alpha03")
+    implementation ("androidx.compose.foundation:foundation:1.5.4")
+    implementation ("androidx.compose.material:material:1.5.4")
 }
