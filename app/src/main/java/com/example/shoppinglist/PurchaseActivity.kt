41efaea46b23690052539eaf28118cd0aff9e582
Anna <annamariaf1999@gmail.com>
2023-11-08 16:05:33 +0100

    Revert "eliminazione button cestino"
    This reverts commit 8b5b97cb181cc8747f1acf53472b6f93ef15845c.

--- app/src/main/java/com/example/shoppinglist/PurchaseActivity.kt
+++ app/src/main/java/com/example/shoppinglist/PurchaseActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -268,20 +269,35 @@ fun ItemRow(item: PurchasableItem,
             )
         }
 
-        Row( modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.End
-        ) {
+        Row( verticalAlignment = Alignment.CenterVertically) {
             // Checkbox
             Checkbox(
                 checked = isChecked,
-                onCheckedChange = { onCheckedChange(it) },
+                onCheckedChange = { onCheckedChange(it) }
             )
+
+            // Bottone Elimina
+            FloatingActionButton(
+                onClick = { onDeleteClick;
+                    mappa[item.category]?.remove(item);
+                    if(mappa[item.category].isNullOrEmpty()){
+                        mappa.remove(item.category)
+                    }},
+                modifier = Modifier.padding(start = 8.dp),
+
+
+                ) {
+                Icon(imageVector = Icons.Outlined.Delete,
+                    contentDescription = "Elimina",
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
 
             }
         }
     }
 
-
+}
 
 
 
